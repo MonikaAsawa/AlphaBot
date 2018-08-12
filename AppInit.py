333581f55f095361beb45a2fd510da2fd2293f15
MonikaAsawa <41234959+MonikaAsawa@users.noreply.github.com>
2018-08-12 19:17:40 -0400

    Add files via upload

--- AppInit.py
+++ AppInit.py
@@ -6,18 +6,19 @@ Created on Sun Aug 12 18:52:07 2018
 """
 
 from DialogFlow import create_entity_type
-from Product import loadProductCategories
+from Product import loadProducts
 
 def setUpApp():
     
     print("setUpApp in AppInit starts")
 
-    productCategories = loadProductCategories()
+    productCategories = loadProducts.keys()
     
     print("Number of productCategories to be loaded",len(productCategories))
     
     #set up entity types
     for productType in productCategories:
+        print(productType)
         create_entity_type("alphabotagent",productType,productType)
         
     #set up entity values in bulk
